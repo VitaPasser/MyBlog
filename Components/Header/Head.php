7734462806acc3d9f547f8fc64390d4c17eebf27
VitaPasse <vitapasser@gmail.com>
2023-06-25 19:46:19 +0300

    added favicon

--- Components/Header/Head.php
+++ Components/Header/Head.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="Components/Header/Source/logo.png" type="image/x-icon">
     <title>миття бліх:
         <?php echo $title_name; ?>
     </title>
